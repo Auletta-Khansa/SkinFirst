1797c95845a913fd272ad644e3e016a55163d7eb
danurrizad <danurrizad242@gmail.com>
2023-03-05 20:53:12 +0700

    update Route for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
-import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/pages/LandingPage";
+import SkinTrivia from "./components/pages/SkinTrivia";
+import InputSkinTrivia from "./components/pages/InputSkinTrivia";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <LandingPage/>
-      <Footer/>
+      <BrowserRouter>
+        <>
+          <Navbar/>
+        </>
+        <Routes>
+          <Route path="/home" element={<LandingPage/>}/>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/skin-trivia" element={<SkinTrivia/>}/>
+          <Route path="/input-trivia" element={<InputSkinTrivia/>}/>
+        </Routes>
+      </BrowserRouter>
+      <>
+        <Footer/>
+      </>
     </>
   );
 }
