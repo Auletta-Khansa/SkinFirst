5cf79813f99b018157652c1a2a05c55ad2414249
danurrizad <danurrizad242@gmail.com>
2023-03-05 20:50:28 +0700

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./src/components/Navbar.jsx",
-    "./src/components/LandingPage.jsx",
+    "./src/components/pages/LandingPage.jsx",
+    "./src/components/pages/SkinTrivia.jsx",
+    "./src/components/pages/InputSkinTrivia.jsx",
     "./src/components/Footer.jsx",
   ],
   theme: {
