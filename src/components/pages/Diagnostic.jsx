aea7c67e56e62d34c0712e7352445be681f1b044
WardatulRadhiyyah <wardatul.radhiyyah@mail.ugm.ac.id>
2023-03-10 22:07:18 +0700

    changes background

--- src/components/pages/Diagnostic.jsx
+++ src/components/pages/Diagnostic.jsx
@@ -3,18 +3,18 @@ const Diagnostic = () => {
   return (
     <>
       {/*Fun Facts */}
-      <section className="min-h-screen  px-40 pt-40 pb-10 bg-[#F0EFE1]">
+      <section className="min-h-screen  px-40 pt-40 pb-10 bg-[#FFFFFFF]">
         <h1 className="text-center text-[50px] font-medium">
           Skin Diseases Diagnosis
         </h1>
         <div className="pt-10 flex justify-between gap-10">
-          <div className="flex h-full bg-[#00695B] w-1/2 px-4 pb-6 text-[20px] shadow-xl rounded-xl">
+          <div className="flex h-full bg-[#013B31] w-1/2 px-4 pb-6 text-[20px] shadow-xl rounded-xl">
             <h1 className="text-start text-white p-4">Upload Image</h1>
             
 
             <div></div>
           </div>
-          <div className="bg-[#00695B] w-1/2 px-4 pb-6 text-[20px]  shadow-xl rounded-xl">
+          <div className="bg-[#013B31] w-1/2 px-4 pb-6 text-[20px]  shadow-xl rounded-xl">
             <h1 className="text-white p-4">Hasil Diagnosis</h1>
             
            <div className="px-4">
