22c4b893e57bece00b77b4a9c04724b0f33630ca
danurrizad <danurrizad242@gmail.com>
2023-03-12 15:41:46 +0700

    fix a better padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,20 +8,18 @@ const Navbar = () => {
       <div className='bg-[#D4D2C5] py-4 fixed w-screen z-50'>
           <div className='px-40'>
             <div className='flex justify-between items-center gap-10 font-bold'>
-                <div>
-                  <Link to='/home' className='text-[28px]'><img src={ImgLogo} alt="img-logo" className='w-1/2' /></Link>
-                </div>
-                <div className='flex justify-end gap-10 text-[15px]'>
-                  <ul className='flex justify-center gap-10 py-2 text-[#011A16]'>
-                    <li><Link to='/home' className='hover:text-[#011a1671]'>Home</Link></li>
-                    <li><Link to='/skin-trivia' className='hover:text-[#011a1671]'>Skin Trivia</Link></li>
-                    <li><Link to='/diagnostic' className='hover:text-[#011a1671]'>Diagnostic</Link></li>
-                    <li><Link to='#home' className='hover:text-[#011a1671]'>Profile</Link></li>
-                  </ul>
-                  <div className='flex items-center'>
-                    <Link to='/login' className='bg-[#F0EFE1] hover:bg-[#c6c4a9] shadow-lg duration-200 px-10 py-2 rounded-xl'><span className='text-[#00695B]'>Login</span></Link>
-                  </div>
-                </div>
+              <div>
+                <Link to='/home' className='text-[28px]'><img src={ImgLogo} alt="img-logo" className='w-1/3' /></Link>
+              </div>
+              <div className='flex justify-end gap-10 text-[15px]'>
+                <ul className='flex gap-6 px-2 py-2 text-[#011A16]'>
+                  <li><Link to='/home' className='hover:text-[#011a1671]'>Home</Link></li>
+                  <li><Link to='/skin-trivia' className='hover:text-[#011a1671]'>Skin Trivia</Link></li>
+                  <li><Link to='/diagnostic' className='hover:text-[#011a1671]'>Diagnostic</Link></li>
+                  <li><Link to='#home' className='hover:text-[#011a1671]'>Profile</Link></li>
+                  <li><Link to='/login' className='bg-[#F0EFE1] hover:bg-[#c6c4a9] shadow-lg duration-200 px-10 py-2 rounded-xl'><span className='text-[#00695B]'>Login</span></Link></li>
+                </ul>
+              </div>
             </div>
           </div>
       </div>
