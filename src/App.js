fdbf40ad03ceec0fd31074a2348844ce0c61aabb
WardatulRadhiyyah <wardatul.radhiyyah@mail.ugm.ac.id>
2023-03-16 20:53:08 +0700

    inputpenyakit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Diagnostic from "./components/pages/Diagnostic";
 import LoginPage from "./components/pages/LoginPage";
 import SignUpPage from "./components/pages/SignUpPage";
 import ProfilePage from "./components/pages/ProfilePage";
+import InputPenyakitPage from "./components/pages/InputPenyakitPage";
+import HistoryPage from "./components/pages/HistoryPage";
 import NotFound from "./components/pages/NotFound";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -26,6 +28,8 @@ function App() {
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/signup" element={<SignUpPage/>}/>
           <Route path="/profile" element={<ProfilePage/>}/>
+          <Route path="/inputpenyakit" element={<InputPenyakitPage/>}/>
+          <Route path="/History" element={<HistoryPage/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
         <Footer/>
