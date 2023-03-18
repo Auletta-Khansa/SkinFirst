5d2593607f3f252f188f769259aff8ae2bd0b751
WardatulRadhiyyah <wardatul.radhiyyah@mail.ugm.ac.id>
2023-03-18 20:23:26 +0700

    historypage

--- src/components/pages/HistoryPage.jsx
+++ src/components/pages/HistoryPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 const History = () => {
   return (
     <>
@@ -9,64 +10,66 @@ const History = () => {
             <h1 className="text-primary-3 text-xl font-bold  py-4 md:text-2xl leading-tight tracking-tight text-center">
               Medical Histories
             </h1>
-          
-            <button
+            <div class="flex w-full justify-end pt-4">
+              <button
                 type="submit"
-                className=" w-full h-1/6 text-primary-0  bg-[#3BB8AA] hover:bg-primary-1 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center"
-                fdprocessedid="gkbxyo"
-              >
-                Save History
+                className=" w-full h-1/4 text-primary-0  bg-[#3BB8AA] hover:bg-primary-1 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-8 text-center"
+                fdprocessedid="gkbxyo">
+                
               </button>
+              </div>
             
 
             <div className=" pt-8">
-            <button
+             <button
                 type="submit"
-                className=" w-full h-1/6 text-primary-0  bg-[#3BB8AA] hover:bg-primary-1 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center"
-                fdprocessedid="gkbxyo"
-              >
-                Save History
+                className=" w-full h-1/6 text-primary-0  bg-[#3BB8AA] hover:bg-primary-1 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-8 text-center"
+                fdprocessedid="gkbxyo">
+                
               </button>
             </div>
 
             <div className=" pt-8 ">
-            <button
+              <button
                 type="submit"
-                className=" w-full h-1/6 text-primary-0  bg-[#3BB8AA] hover:bg-primary-1 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center"
-                fdprocessedid="gkbxyo"
-              >
-                Save History
+                className=" w-full h-1/6 text-primary-0  bg-[#3BB8AA] hover:bg-primary-1 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-8 text-center"
+                fdprocessedid="gkbxyo">
+                
               </button>
             </div>
             
         </div>
 
-          <div className="bg-[#013B31] w-1/2 px-4 pb-6 text-[20px]  shadow-xl rounded-xl">
-           
+        <div className="bg-[#013B31] w-2/3 px-4 pb-6 text-[20px]  shadow-xl rounded-xl">
+            <div className=" w-2/3 px-4 py-4">
+                <h1 className="text-primary-3 py-2 text-lg font-bold md:text-2xl leading-tight tracking-tight ">
+                Disease Name
+                </h1>
+                <div className=" px-4 py-8 bg-primary-1 rounded-lg border ">
+                <span className="text-teal-900"></span>
+              </div>
+            </div>
 
-            <div className="px-4 pt-8">
-              <div className=" px-4 py-2 bg-primary-1 rounded-lg border ">
-                <span className="text-teal-900 item-center justify-center">
-                Diagnostic Results : 
-                </span>
-              </div>
-            </div>
-            <div className="px-4 py-6">
-              <div className=" px-4 py-16 bg-primary-1 rounded-lg border ">
-                <span className="text-teal-900">
+            <div className="w-2/3 px-4 py-4">
+                <h1 className="text-primary-3 py-2 text-lg font-bold md:text-2xl leading-tight tracking-tight ">
                     Description
-                </span>
+                </h1>
+                <div className=" px-4 py-16 bg-primary-1 rounded-lg border ">
+                <span className="text-teal-900"></span>
               </div>
             </div>
-            <div className="px-4 ">
-              <div className=" px-4 py-16 bg-primary-1 rounded-lg border ">
-                <span className="text-teal-900 text-start">
-                    Treatment Recommendation
-                </span>
+            
+            
+            <div className="w-2/3 px-4 py-4">
+                <h1 className="text-primary-3 py-2 text-lg font-bold md:text-2xl leading-tight tracking-tight ">
+                Treatment Recomendation
+                </h1>
+                <div className=" px-4 py-16 bg-primary-1 rounded-lg border ">
+                <span className="text-teal-900"></span>
               </div>
             </div>
+            </div>  
           </div>
-        </div>
       </section>
     </>
   );
