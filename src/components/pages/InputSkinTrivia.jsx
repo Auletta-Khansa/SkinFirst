52029df0fad86e60f8176468b84db7a4bc758bdf
danurrizad <danurrizad242@gmail.com>
2023-04-30 00:20:20 +0700

    style: add responsive design

--- src/components/pages/InputSkinTrivia.jsx
+++ src/components/pages/InputSkinTrivia.jsx
@@ -4,7 +4,7 @@ import { FaRegWindowClose } from "react-icons/fa"
 import { create as ipfsHttpClient } from 'ipfs-http-client'
 import { Buffer } from 'buffer';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 //import env from 'react-dotenv';
 
 const InputSkinTrivia = () => {
@@ -79,13 +79,13 @@ const InputSkinTrivia = () => {
 
   return (
     <>
-        <div className='min-h-screen px-40 py-40 font-merriweather'>
-            <h1 className='text-[36px] font-bold text-center pb-10 font-poppins'>List a New Skin Trivia Information</h1>
+        <div className='min-h-screen 2xl:px-40 xl:px-40 lg:px-40 px-10 py-40 font-merriweather'>
+            <h1 className='2xl xl:text-[36px] lg:text-[36px] md:text-[36px] text-[30px] font-bold text-center pb-10 font-poppins'>List a New Skin Trivia Information</h1>
             <div className='flex justify-center'>
-                <form onSubmit={inputTrivia} className='bg-primary-0 p-8 w-1/2 shadow-2xl rounded-xl'>
+                <form onSubmit={inputTrivia} className='bg-primary-0 p-8 2xl:w-1/2 xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-2/3 w-full shadow-2xl rounded-xl'>
                     {/* IMAGE */}
                     <div className='flex-col flex text-white pb-8'>
-                        <h2 className='text-medium text-[24px]'>Image</h2>
+                        <h2 className='text-medium 2xl:text-[24px] xl:text-[24px] lg:text-[24px] md:text-[24px] text-[18px]'>Image</h2>
                         <div class="relative w-64 h-64 border-2 border-dashed border-gray-300 bg-white rounded-lg">
                             <input type="file" id="file-input" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer" onChange={uploadToIPFS}/>
                             <label for="file-input" class="z-absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center items-center text-gray-500 text-lg">
@@ -108,33 +108,35 @@ const InputSkinTrivia = () => {
 
                     {/* NAME */}
                     <div className='flex-col flex text-white pb-8'>
-                        <label className='text-medium text-[24px]' >Name</label>
-                        <input type="text" className='bg-white text-black px-4 py-2' onChange={(e)=>setName(e.target.value)}/>
+                        <label className='text-medium 2xl:text-[24px] xl:text-[24px] lg:text-[24px] md:text-[24px] text-[18px]' >Name</label>
+                        <input type="text" className='bg-white text-black rounded px-4 py-2 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] text-[11px]' onChange={(e)=>setName(e.target.value)}/>
                     </div>
 
                     {/* DESCRIPTION */}
                     <div className='flex-col flex text-white pb-2'>
-                        <label className='text-medium text-[24px]' >Description</label>
+                        <label className='text-medium 2xl:text-[24px] xl:text-[24px] lg:text-[24px] md:text-[24px] text-[18px]' >Description</label>
                         {description.map((input, index) => (
-                            <div key={index} className="flex items-center mt-4 justify-start ">
-                                <label htmlFor={`description-${index}`} className="pr-6 w-[60%]">
+                            <div key={index} className="flex-col items-center mt-2 justify-start ">
+                                <label htmlFor={`description-${index}`} className="pr-6 w-[60%] 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] text-[11px]">
                                     Description {index + 1}:
                                 </label>
-                                <input
-                                    id={`description-${index}`}
-                                    type="text"
-                                    value={input}
-                                    onChange={(event) => handleInputChange(index, event)}
-                                    className="border border-gray-300 w-full rounded px-4 py-2 mr-4 text-black"
-                                />
-                                {description.length > 1 && (
-                                    <button
-                                    onClick={() => handleRemoveInput(index)}
-                                    className="bg-red-500 text-white rounded px-0.5 text-[30px]"
-                                    >
-                                    <FaRegWindowClose/>
-                                    </button>
-                                )}
+                                <div className='flex items-center justify-start '>
+                                    <input
+                                        id={`description-${index}`}
+                                        type="text"
+                                        value={input}
+                                        onChange={(event) => handleInputChange(index, event)}
+                                        className="border border-gray-300 w-full rounded px-4 py-2 mr-4 text-black 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] text-[11px]"
+                                    />
+                                    {description.length > 1 && (
+                                        <button
+                                        onClick={() => handleRemoveInput(index)}
+                                        className="bg-red-500 text-white rounded px-0.5 text-[30px]"
+                                        >
+                                        <FaRegWindowClose/>
+                                        </button>
+                                    )}
+                                </div>
                             </div>
                         ))}
                         <button onClick={handleAddInput} className="bg-primary-2 hover:bg-primary-3 hover:text-primary-2 duration-200 active:bg-primary-2 active:text-white text-white rounded px-4 py-2 mt-4">
@@ -144,8 +146,8 @@ const InputSkinTrivia = () => {
 
                     {/* SUBMIT */}
                     <div className='flex justify-end gap-4 font-medium mt-20'>
-                        <button className='bg-[#ef4d4d]  rounded-lg px-10 py-2 shadow-xl hover:bg-[#cf2b2b] duration-200 text-white'>Reset</button>
-                        <button className='bg-[#F0EFE1]  rounded-lg px-10 py-2 shadow-xl hover:bg-[#c8c7bb] duration-200' type='submit'>Submit</button>
+                        <Link to="/skin-trivia" className='bg-[#ef4d4d] rounded-lg 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-6 py-2 shadow-xl hover:bg-[#cf2b2b] duration-200 text-white'>Cancel</Link>
+                        <button className='bg-[#F0EFE1] rounded-lg 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-6 py-2 shadow-xl hover:bg-[#c8c7bb] duration-200' type='submit'>Submit</button>
                     </div>
                 </form>
             </div>
--- src/components/pages/SkinTrivia.jsx
+++ src/components/pages/SkinTrivia.jsx
@@ -24,35 +24,35 @@ const SkinTrivia = () => {
     return (
     <>
     {/*Definition */}
-    <section className='min-h-[50vh] px-40 pt-40 font-poppins'>
+    <section className='min-h-[50vh] 2xl:px-40 xl:px-40 lg:px-40 px-10 pt-40 font-poppins'>
         <div className='flex-col justify-center'>
             <h1 className='text-center text-[50px] font-bold pb-10'>Skin Trivia</h1>
             <div className='items-center flex justify-center'>
                 <img src={ImageSkinTrivia} alt='skin-trivia'/>
             </div>
-            <p className='text-center text-[24px] px-40'>Various interesting and fun facts related to skin, skin care tips, skin conditions, and more.</p>
+            <p className='text-center text-[24px] 2xl:px-40 xl:px-40 lg:px-40 px-10'>Various interesting and fun facts related to skin, skin care tips, skin conditions, and more.</p>
         </div>
         
     </section>
 
     {/*Fun Facts */}
-    <section className='min-h-screen  px-40 pt-40 font-merriweather'>
+    <section className='min-h-screen 2xl:px-40 xl:px-40 lg:px-40 px-10 pt-40 font-merriweather'>
         <h1 className='text-center text-[50px] font-medium font-poppins'>Skin Diseases</h1>
         <div className='pt-10 flex flex-col justify-center gap-4'>
-            <div className='text-white flex justify-center gap-20'>
-                <button onClick={()=>handleTrivia(1)} className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
-                <button className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
-                <button className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+            <div className='text-white flex justify-center 2xl:gap-20 xl:gap-20 lg:gap-20 md:gap-20 gap-3'>
+                <button onClick={()=>handleTrivia(1)} className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+                <button className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+                <button className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
             </div>
-            <div className='text-white flex justify-center gap-20'>
-                <button className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
-                <button className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
-                <button className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+            <div className='text-white flex justify-center 2xl:gap-20 xl:gap-20 lg:gap-20 md:gap-20 gap-3'>
+                <button className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+                <button className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+                <button className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
             </div>
-            <div className='text-white flex justify-center gap-20'>
-                <button className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
-                <button className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
-                <button className='bg-primary-0 px-10 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+            <div className='text-white flex justify-center 2xl:gap-20 xl:gap-20 lg:gap-20 md:gap-20 gap-3'>
+                <button className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+                <button className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
+                <button className='bg-primary-0 2xl:text-[18px] xl:text-[18px] lg:text-[18px] md:text-[18px] text-[10px] 2xl:px-10 xl:px-10 lg:px-10 md:px-10 px-4 py-2 rounded-full hover:bg-primary-3 hover:text-primary-0 duration-300 focus:bg-primary-3 focus:text-primary-0 active:bg-primary-1'>Atopic Dermatitis</button>
             </div>
             {/* <div className='bg-[#013B31] w-1/3 p-4 text-[20px] text-white shadow-xl rounded-xl'>
                 <img src={AtopicDermatitis} alt='atopic-dermatitis' className='border-white border-2 border-solid'/>
@@ -77,8 +77,8 @@ const SkinTrivia = () => {
                     {showTrivia.one ? (
                     <>
                         <h1 className='text-[30px] font-medium pb-8'>Atopic Dermatitis</h1>
-                        <div className='flex gap-10'>
-                            <img src={AtopicDermatitis} alt='atopic-dermatitis' className='border-white border-2 border-solid w-1/2'/>
+                        <div className='flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-row flex-col gap-10'>
+                            <img src={AtopicDermatitis} alt='atopic-dermatitis' className='border-white border-2 border-solid 2xl:w-1/2 xl:w-1/2 lg:w-1/2 md:w-1/2 w-full'/>
                             <div className='flex-col gap-4 flex'>
                                 <div className='flex gap-1'>
                                     <div>-</div>
